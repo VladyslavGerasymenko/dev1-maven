0983df3d207715d358cbbf71ecc36ba0dc9899f1
VladyslavGerasymenko <jeorgebradford@gmail.com>
2023-07-23 21:57:13 +0300

    Initial commit

---
+++ src/main/java/org/example/Name.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Name {
+    private String firstName;
+    private String lastName;
+
+}
